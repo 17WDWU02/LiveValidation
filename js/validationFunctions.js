0e590103298b74f216b09e06a132ca6c53f2661f
Richard Hpa <richard.hpa@hotmail.com>
2017-05-30 12:16:06 +1200

    Added Comments to the functions

--- js/validationFunctions.js
+++ js/validationFunctions.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	//By default all of the input fields won't be valid as they would be empty
+	//Set all varaibles to false
 	var ValidFirstName = false;
 	var ValidLastName = false;
 	var ValidEmail = false;
@@ -9,11 +11,18 @@ $(document).ready(function(){
 	//When the submit button is pressed
 	$("#submitButton").click(function(event){
 		event.preventDefault();
+		//The form should only submit when all of the input field variables are true
 	});
 
-
+	//Validate First name
+	//Both the sections in the focus and the keyup function run the same thing
+	//even though they are written differentlu
 	$("#firstName")
 		.focus(function(){
+			//For if statements you dont need to give a = value
+			//If you leave it then the code will assume you want it to be true
+			//if(x=true) and if(x) both mean the same thing and are expecting the value of true
+			//This line calls the required function and passes through the current element (Go to required function) 
 			if(required($(this))){
 				ValidFirstName = false;
 				return;
@@ -41,45 +50,28 @@ $(document).ready(function(){
 			minLen($(this), 10);
 		})
 
-	//Validate Email
-	$("#email")
-		.focus(function(){
-
-		}).blur(function(){
-
-		}).keyup(function(){
-
-		});
-
-	//Validate Password
-	$("#password")
-		.focus(function(){
-
-		}).blur(function(){
-
-		}).keyup(function(){
-
-		});
-
-	//Validate Confirm Password
-	$("#confirmPassword")
-		.focus(function(){
-
-		}).blur(function(){
-
-		}).keyup(function(){
-
-		});
-
 
 });
 
 function required(element){
+	//the required function is looking for a value to be passed through it
+	//we have been passing $(this) which means that whatever value you pass through it becomes element
 	if(element.val().length === 0){
+		//Check to see if there is anything in the input field
+		//If there isnt then add the error code
 		element.parent().find('span.input-errors').text('This field is required');
+		//Because we are writing a short hand if statement, it is looking for a value of true to be able
+		//to continue with the statement.
+		//What we are doing is we want to return a value back to it and it should be true or false
+		//if we return true then we are saying that there is errors so then whatever is in the if statement will run
 		return true;
 	} else {
+		//If the value is more than 0 characters then we want to empty the span
 		element.parent().find('span.input-errors').empty();
+		//Because we are writing a short hand if statement, it is looking for a value of true to be able
+		//to continue with the statement.
+		//What we are doing is we want to return a value back to it and it should be true or false
+		//if we return false then we are saying that there are no errors so then whatever is in the if statement won't run
 		return false;
 	}
 }
